639c31eef792ab8a8333adb12f5074bc0dc1739e
Zuru07 <suraj.manivannan@gmail.com>
2025-11-03 15:07:39 +0530

    App Changes

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ const htmlContent = `
   <h1>CI/CD is a Success! </h1>
   <p>Your changes deployed effortlessly on GCP Cloud Run.</p>
   <button class="button" onclick="window.location.reload()">Refresh Status</button>
-  <footer>Powered by Express & Google Cloud Platform</footer>
+  <footer>Powered by Express & GCP </footer>
 </body>
 </html>
 `;
